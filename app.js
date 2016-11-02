4c4994b1f8c0b8911eeea4ba5d9a86c36f188252
Tom <tuomis29@gmail.com>
2016-11-02 23:41:15 +0000

    finished api

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
 var express = require('express')
 var app = express()
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+var monthNames = ["January", "February", "March", "April", "May", "June",
+  "July", "August", "September", "October", "November", "December"
+];
+
+app.get('/:id?', function(req, res) {
+    var date = false;
+    if(req.params.id !== undefined){
+        if(!isNaN(parseInt(req.params.id))){
+            date =new Date(req.params.id*1000);
+        }else {
+            date =new Date(req.params.id);
+        }
+    }
+    
+    var date1 = (date)?Math.floor(date.getTime()/1000):null;
+    var date2 = (date)?(monthNames[date.getMonth()]+" "+date.getDate()+", "+date.getFullYear()):null;
+    res.send({ "unix": date1, "natural": date2 });
+});
 
 app.listen(process.env.PORT,process.env.IP, function () {
   console.log('Example app listening on port '+process.env.PORT);
-})
+});
